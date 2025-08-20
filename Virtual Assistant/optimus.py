6f8902259746b6971caa3ae24c4459f184322984
Taha <ta753090@gmail.com>
2025-08-20 18:40:48 -0400

    Update optimus.py

--- Virtual Assistant/optimus.py
+++ Virtual Assistant/optimus.py
@@ -11,37 +11,38 @@ engine.setProperty('voice', voices[0].id) # index 0 makes male voice, index 1 ma
 
 def speak(text):
     engine.say(text)
-    
     engine.runAndWait()
+
+
 def processCommand(c):
-    if "open google" in c.lower():
-        webbrowser.open("https://google.com")
-    elif "open facebook" in c.lower():
-        webbrowser.open("https://facebook.com")
-    elif "open instagram" in c.lower():
-        webbrowser.open("https://instagram.com")
-    elif "open github" in c.lower():
-        webbrowser.open("https://github.com") 
-    elif "open youtube" in c.lower():
-        webbrowser.open("https://youtube.com")
+    pass
+    #if "open google" in c.lower():
+        #webbrowser.open("https://google.com")
+    #elif "open facebook" in c.lower():
+        #webbrowser.open("https://facebook.com")
+    #elif "open instagram" in c.lower():
+        #webbrowser.open("https://instagram.com")
+    #elif "open github" in c.lower():
+        #webbrowser.open("https://github.com") 
+    #elif "open youtube" in c.lower():
+        #webbrowser.open("https://youtube.com")
 
 
 if __name__ == "__main__":
     speak("Initializing Optimus.......")
     while True:
-        
         r = sr.Recognizer()
         
         print("recognizing....")
         try:
-           with sr.Microphone() as source:
+            with sr.Microphone() as source:
                
-               print("Listening...")
-               audio = r.listen(source, timeout=2, phrase_time_limit=1)
-           word = r.recognize_google(audio)
-           if(word.lower() == "optimus"):
-               speak("Yes Chief")
-               with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "optimus"):
+                speak("Yes Chief")
+                with sr.Microphone() as source:
                    print("Optimus Activated...")
                    audio = r.listen(source)
                    command = r.recognize_google(audio)
