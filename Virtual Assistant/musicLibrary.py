2a79c74daa8dcc129af962c2fdb9388f6def7f7f
Taha <ta753090@gmail.com>
2025-08-23 15:19:05 -0400

    File added

--- Virtual Assistant/musicLibrary.py
+++ Virtual Assistant/musicLibrary.py
@@ -2,10 +2,4 @@ music = {
     "sprinter": "https://youtu.be/pSY3i5XHHXo?si=2eI9ECrsR8Esgoc3",
     "stacks": "https://youtu.be/ZoNH1HJr0OQ?si=8HytP5YVujJscgxy",
     "weeknd": "https://youtu.be/4NRXx6U8ABQ?si=TzhcAK2PnONTrur6"
-
-
-
-
-
-
 }
